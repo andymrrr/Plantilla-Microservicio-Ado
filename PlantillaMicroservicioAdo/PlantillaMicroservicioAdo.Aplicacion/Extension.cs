67e2a11160b6ce98d07a4cf91969f0c7a6396540
Andy Reyes <42513348+andymrrr@users.noreply.github.com>
2025-01-14 10:57:17 -0400

    fix

--- PlantillaMicroservicioAdo/PlantillaMicroservicioAdo.Aplicacion/Extension.cs
+++ PlantillaMicroservicioAdo/PlantillaMicroservicioAdo.Aplicacion/Extension.cs
@@ -1,7 +1,22 @@
-﻿namespace PlantillaMicroservicioAdo.Aplicacion
-{
-    public class Class1
-    {
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlantillaMicroservicioAdo.Aplicacion.Mapeo;
+using System.Reflection;
 
+namespace PlantillaMicroservicioAdo.Aplicacion
+{
+    public static class Extension
+    {
+        public static void AddServicioAplicacion(this IServiceCollection servicio, IConfiguration config)
+        {
+            servicio.AddMediatR(opcion => opcion.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            var cofiguracionMapeo = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MapeoPerfil());
+            });
+            IMapper mapeo = cofiguracionMapeo.CreateMapper();
+            servicio.AddSingleton(mapeo);
+        }
     }
 }
